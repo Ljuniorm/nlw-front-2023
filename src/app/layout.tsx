b45403bbc8763f5107d99282910c53bfaeee0120
Luiz Junior <luiz.junior@blueticket.com.br>
2023-05-21 23:23:35 -0300

    List memories

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         className={`${roboto.variable} ${bai.variable} bg-gray-900 font-sans text-gray-100`}
       >
         <main className="grid min-h-screen grid-cols-2">
-          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+          <div className="relative flex  flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
             <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"></div>
 
             <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"></div>
@@ -43,7 +43,7 @@ export default function RootLayout({
             <Copyright />
           </div>
 
-          <div className=" flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+          <div className="flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover">
             {children}
           </div>
         </main>
